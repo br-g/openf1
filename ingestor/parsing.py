c82e0fe481f9ec5c74acda6274be221da6770ecc
Preston Hager <preston@hagerfamily.com>
2024-04-07 18:08:12 -0600

    Added color based string parsing to ingestor

--- ingestor/parsing.py
+++ ingestor/parsing.py
@@ -169,12 +169,17 @@ def _cast(s: str) -> Union[str, datetime, bool, int, float]:
         return s
     return _try_parse_number(s)
 
+@lru_cache(maxsize=None)
+def _cast_color(s: str) -> str:
+    """Casts a color string to a standard hex format"""
+    return '#' + s
+
 def cast(obj: Any) -> Any:
     """Casts `obj` to the most specific type possible, recursively"""
     if isinstance(obj, str):
         return _cast(obj)
     elif isinstance(obj, dict):
-        return {k: cast(v) for k, v in obj.items()}
+        return {k: _cast_color(v) if k.lower().endswith('color') or k.lower().endswith('colour') else cast(v) for k, v in obj.items()}
     elif isinstance(obj, list):
         return [cast(e) for e in obj]
     else:
