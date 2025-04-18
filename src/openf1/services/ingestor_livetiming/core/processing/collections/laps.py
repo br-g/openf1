c9d73cba63c76c226a9ee59e66419c46a0acc3d0
br-g <brgo@mail.com>
2025-04-18 10:06:34 +0200

    Fix laps collection (ignore data before session)

--- src/openf1/services/ingestor_livetiming/core/processing/collections/laps.py
+++ src/openf1/services/ingestor_livetiming/core/processing/collections/laps.py
@@ -53,8 +53,9 @@ def _is_lap_valid(lap: Lap) -> bool:
 @dataclass
 class LapsCollection(Collection):
     name = "laps"
-    source_topics = {"TimingData"}
+    source_topics = {"TimingAppData", "TimingData"}
 
+    is_session_started: bool = False
     laps: defaultdict = field(default_factory=lambda: defaultdict(list))
     updated_laps: set = field(default_factory=set)  # laps updated since last message
 
@@ -112,119 +113,123 @@ class LapsCollection(Collection):
             return
 
         for driver_number, data in message.content["Lines"].items():
-            try:
-                driver_number = int(driver_number)
-            except:
-                continue
+            if message.topic == "TimingAppData" and "Stints" in data:
+                self.is_session_started = True
 
-            if not isinstance(data, dict):
-                continue
+            elif self.is_session_started and message.topic == "TimingData":
+                try:
+                    driver_number = int(driver_number)
+                except:
+                    continue
 
-            try:
-                lap_time = data.get("LastLapTime", {}).get("Value")
-            except:
-                lap_time = None
-            if isinstance(lap_time, timedelta):
-                self._update_lap(
-                    driver_number=driver_number,
-                    property="lap_duration",
-                    value=lap_time.total_seconds(),
-                )
+                if not isinstance(data, dict):
+                    continue
 
-            sectors = data.get("Sectors")
-            if isinstance(sectors, dict):
-                for sector_number_s, sector_data in sectors.items():
-                    try:
-                        sector_number = int(sector_number_s) + 1
-                    except:
-                        continue
+                try:
+                    lap_time = data.get("LastLapTime", {}).get("Value")
+                except:
+                    lap_time = None
+                if isinstance(lap_time, timedelta):
+                    self._update_lap(
+                        driver_number=driver_number,
+                        property="lap_duration",
+                        value=lap_time.total_seconds(),
+                    )
 
-                    if not isinstance(sector_data, dict):
-                        continue
-
-                    if "Value" in sector_data:
+                sectors = data.get("Sectors")
+                if isinstance(sectors, dict):
+                    for sector_number_s, sector_data in sectors.items():
                         try:
-                            duration = float(sector_data["Value"])
-                        except:
-                            duration = None
-                        if duration is not None:
-                            self._update_lap(
-                                driver_number=driver_number,
-                                property=f"duration_sector_{sector_number}",
-                                value=duration,
-                            )
-                            lap = self._get_latest_lap(driver_number)
-                            if (
-                                not lap.lap_duration
-                                and lap.duration_sector_1
-                                and lap.duration_sector_2
-                                and lap.duration_sector_3
-                            ):
-                                self._update_lap(
-                                    driver_number=driver_number,
-                                    property="lap_duration",
-                                    value=round(
-                                        lap.duration_sector_1
-                                        + lap.duration_sector_2
-                                        + lap.duration_sector_3,
-                                        3,
-                                    ),
-                                )
-
-                    if "Segments" in sector_data:
-                        segments_data = sector_data["Segments"]
-                        if isinstance(segments_data, dict):
-                            for (
-                                segment_number,
-                                segment_data,
-                            ) in segments_data.items():
-                                try:
-                                    segment_number = int(segment_number)
-                                except:
-                                    continue
-                                if not isinstance(segment_data, dict):
-                                    continue
-                                self._add_segment_status(
-                                    driver_number=driver_number,
-                                    sector_number=sector_number,
-                                    segment_number=segment_number,
-                                    segment_status=segment_data.get("Status"),
-                                )
-
-            speeds = data.get("Speeds")
-            if isinstance(speeds, dict):
-                for label, speed_data in speeds.items():
-                    if not isinstance(label, str):
-                        continue
-                    if not isinstance(speed_data, dict):
-                        continue
-                    if label == "ST" or label.startswith("I"):
-                        try:
-                            value = int(speed_data.get("Value"))
+                            sector_number = int(sector_number_s) + 1
                         except:
                             continue
-                        self._update_lap(
-                            driver_number=driver_number,
-                            property=f"{label.lower()}_speed",
-                            value=value,
-                        )
 
-            if data.get("NumberOfLaps") is not None:
-                latest_lap = self._get_latest_lap(driver_number=driver_number)
-                if _is_lap_valid(latest_lap):
-                    self._add_lap(driver_number=driver_number)
-                self._update_lap(
-                    driver_number=driver_number,
-                    property="date_start",
-                    value=message.timepoint,
-                )
+                        if not isinstance(sector_data, dict):
+                            continue
 
-            if data.get("PitOut") is not None:
-                self._update_lap(
-                    driver_number=driver_number,
-                    property="is_pit_out_lap",
-                    value=True,
-                )
+                        if "Value" in sector_data:
+                            try:
+                                duration = float(sector_data["Value"])
+                            except:
+                                duration = None
+                            if duration is not None:
+                                self._update_lap(
+                                    driver_number=driver_number,
+                                    property=f"duration_sector_{sector_number}",
+                                    value=duration,
+                                )
+                                lap = self._get_latest_lap(driver_number)
+                                if (
+                                    not lap.lap_duration
+                                    and lap.duration_sector_1
+                                    and lap.duration_sector_2
+                                    and lap.duration_sector_3
+                                ):
+                                    self._update_lap(
+                                        driver_number=driver_number,
+                                        property="lap_duration",
+                                        value=round(
+                                            lap.duration_sector_1
+                                            + lap.duration_sector_2
+                                            + lap.duration_sector_3,
+                                            3,
+                                        ),
+                                    )
 
-        yield from deepcopy(self.updated_laps)
-        self.updated_laps = set()
+                        if "Segments" in sector_data:
+                            segments_data = sector_data["Segments"]
+                            if isinstance(segments_data, dict):
+                                for (
+                                    segment_number,
+                                    segment_data,
+                                ) in segments_data.items():
+                                    try:
+                                        segment_number = int(segment_number)
+                                    except:
+                                        continue
+                                    if not isinstance(segment_data, dict):
+                                        continue
+                                    self._add_segment_status(
+                                        driver_number=driver_number,
+                                        sector_number=sector_number,
+                                        segment_number=segment_number,
+                                        segment_status=segment_data.get("Status"),
+                                    )
+
+                speeds = data.get("Speeds")
+                if isinstance(speeds, dict):
+                    for label, speed_data in speeds.items():
+                        if not isinstance(label, str):
+                            continue
+                        if not isinstance(speed_data, dict):
+                            continue
+                        if label == "ST" or label.startswith("I"):
+                            try:
+                                value = int(speed_data.get("Value"))
+                            except:
+                                continue
+                            self._update_lap(
+                                driver_number=driver_number,
+                                property=f"{label.lower()}_speed",
+                                value=value,
+                            )
+
+                if data.get("NumberOfLaps") is not None:
+                    latest_lap = self._get_latest_lap(driver_number=driver_number)
+                    if _is_lap_valid(latest_lap):
+                        self._add_lap(driver_number=driver_number)
+                    self._update_lap(
+                        driver_number=driver_number,
+                        property="date_start",
+                        value=message.timepoint,
+                    )
+
+                if data.get("PitOut") is not None:
+                    self._update_lap(
+                        driver_number=driver_number,
+                        property="is_pit_out_lap",
+                        value=True,
+                    )
+
+                yield from deepcopy(self.updated_laps)
+                self.updated_laps = set()
