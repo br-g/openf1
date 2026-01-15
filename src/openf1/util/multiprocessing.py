7f125dfe29cb4cf02e1595c1b4db1cd78a04ac80
Jeffrey Zhang <jeffreyjpizhang@gmail.com>
2026-01-14 22:33:11 -0800

    move 'multiprocessing' module to 'util'

---
+++ src/openf1/util/multiprocessing.py
@@ -0,0 +1,15 @@
+from concurrent.futures import ProcessPoolExecutor
+
+from typing import Callable, Iterable, Iterator, TypeVar
+
+_S = TypeVar('S')
+_T = TypeVar('T')
+
+
+def map_parallel(fn: Callable[..., _T], *iterables: Iterable[_S], max_workers: int | None = None, chunksize: int = 1) -> Iterator[_T]:
+    """
+    Returns an iterator equivalent to map(fn, iterables), processed in parallel by max_workers workers in tasks of size chunksize.
+    """
+    with ProcessPoolExecutor(max_workers=max_workers) as exec:
+        for result in exec.map(fn, *iterables, chunksize=chunksize):
+            yield result
