8098bb21552cc546ce3bb33772a58d5a79d5fb83
br-g <brgo@mail.com>
2026-01-20 19:06:11 +0100

    WIP

--- src/openf1/services/ingestor_livetiming/core/processing/collections/session_state.py
+++ src/openf1/services/ingestor_livetiming/core/processing/collections/session_state.py
@@ -18,6 +18,7 @@ class SessionState(Document):
     meeting_key: int
     session_key: int
     status: str
+    _state_number: int
     qualifying_phase: int | None = None
     race_lap: int | None = None
     date_start: datetime | None = None
@@ -27,7 +28,7 @@ class SessionState(Document):
     def unique_key(self) -> tuple:
         return (
             self.session_key,
-            self.qualifying_phase if self.qualifying_phase is not None else 1,
+            self._state_number,
         )
 
 
@@ -39,25 +40,45 @@ class SessionStateCollection(Collection):
     current_state: SessionState | None = None
     has_been_updated: bool = False
 
-    def _initialize_state(self):
+    def _reset_state(self):
+        if self.current_state and self.current_state.status == "Scheduled":
+            return
+
         self.current_state = SessionState(
             meeting_key=self.meeting_key,
             session_key=self.session_key,
             status="Scheduled",
+            _state_number=(
+                0
+                if self.current_state is None
+                else self.current_state._state_number + 1
+            ),
+            qualifying_phase=(
+                None
+                if self.current_state is None
+                or self.current_state.qualifying_phase is None
+                else self.current_state.qualifying_phase
+            ),
+            race_lap=(
+                None
+                if self.current_state is None or self.current_state.race_lap is None
+                else self.current_state.race_lap + 1
+            ),
         )
-        self.has_been_updated = True
 
     def _update_state(self, property: str, value: any):
         old_value = getattr(self.current_state, property)
         if value != old_value:
             setattr(self.current_state, property, value)
-            self.has_been_updated = True
+
+            if self.current_state.status != "Scheduled":
+                self.has_been_updated = True
 
     def process_message(self, message: Message) -> Iterator[SessionState]:
         self.has_been_updated = False
 
         if self.current_state is None:
-            self._initialize_state()
+            self._reset_state()
 
         status_series = message.content.get("StatusSeries")
         if status_series:
@@ -70,12 +91,12 @@ class SessionStateCollection(Collection):
                     date = to_datetime(item.get("Utc"))
                     if date is not None:
                         date = pytz.utc.localize(date)
-                        date = date.replace(microsecond=0)
 
-                    if status == "Started":
-                        self._update_state(property="date_end", value=None)
-                        if self.current_state.date_start is None:
-                            self._update_state(property="date_start", value=date)
+                    if status == "Inactive":
+                        continue
+                    elif status == "Started" and date != self.current_state.date_start:
+                        self._reset_state()
+                        self._update_state(property="date_start", value=date)
                     elif status in {"Finished", "Aborted"}:
                         self._update_state(property="date_end", value=date)
 
@@ -93,13 +114,13 @@ class SessionStateCollection(Collection):
             for item in series:
                 qualifying_phase = item.get("QualifyingPart")
                 if qualifying_phase is not None:
-                    self._initialize_state()
+                    self._reset_state()
                     self._update_state(
                         property="qualifying_phase", value=qualifying_phase
                     )
 
                 race_lap = item.get("Lap")
-                if race_lap is not None:
+                if race_lap is not None and self.current_state != "Aborted":
                     self._update_state(property="race_lap", value=race_lap)
 
         if self.has_been_updated:
