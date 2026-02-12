568721a03efb40b33a41e88e02abd549eddb58ca
Bruno Godefroy <brgo@mail.com>
2026-02-12 16:10:11 +0100

    Handle new signalcore raw data stream

--- src/openf1/services/ingestor_livetiming/real_time/recording.py
+++ src/openf1/services/ingestor_livetiming/real_time/recording.py
@@ -3,7 +3,9 @@ import asyncio
 from loguru import logger
 
 
-async def record_to_file(filepath: str, topics: list[str], timeout: int):
+async def record_to_file(
+    filepath: str, topics: list[str], timeout: int, is_authenticated: bool
+):
     """Records raw F1 data to a file, using a slightly modified version of the FastF1
     live timing module (https://github.com/br-g/fastf1-livetiming)
     """
@@ -12,23 +14,22 @@ async def record_to_file(filepath: str, topics: list[str], timeout: int):
             command = (
                 ["python", "-m", "fastf1_livetiming", "save", filepath]
                 + sorted(list(topics))
+                + (["--auth"] if is_authenticated else [])
                 + ["--timeout", str(timeout)]
             )
-            proc = await asyncio.create_subprocess_exec(
-                *command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-            )
+            proc = await asyncio.create_subprocess_exec(*command)
 
-            stdout, stderr = await proc.communicate()
+            # Wait for the process to complete
+            await proc.wait()
 
             # Check if the process exited cleanly with an exit code of 0.
             if proc.returncode == 0:
-                logger.info(f"Stdout: {stdout.decode().strip()}")
+                logger.info("Recorder subprocess completed successfully.")
                 break
             else:
                 logger.error(
                     f"Recorder subprocess failed with exit code {proc.returncode}."
                 )
-                logger.error(f"Stderr: {stderr.decode().strip()}")
         except Exception:
             logger.exception(
                 "An unexpected exception occurred while trying to run "
